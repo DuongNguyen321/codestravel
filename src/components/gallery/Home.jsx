a7c1abed5a4bb7eef83af69f06e27e99861799dc
NGUYEN MINH NHAT DUONG <duongcoilc2004@gmail.com>
2022-03-08 01:11:03 +0700

    gallery using api

--- src/components/gallery/Home.jsx
+++ src/components/gallery/Home.jsx
@@ -1,53 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const HomeHeading = [
-  { id: 1, text: "T", className: "" },
-  { id: 2, text: "Ổ", className: "" },
-  { id: 3, text: "N", className: "" },
-  { id: 4, text: "G", className: "" },
-  { id: 5, text: "", className: "space" },
-  { id: 6, text: "H", className: "" },
-  { id: 7, text: "Ợ", className: "" },
-  { id: 8, text: "P", className: "" },
-];
-const HomeBox = [
-  {
-    id: 1,
-    src: "../images/Home/g-1.jpg",
-  },
-  {
-    id: 2,
-    src: "../images/Home/g-2.jpg",
-  },
-  {
-    id: 3,
-    src: "../images/Home/g-3.jpg",
-  },
-  {
-    id: 4,
-    src: "../images/Home/g-4.jpg",
-  },
-  {
-    id: 5,
-    src: "../images/Home/g-5.jpg",
-  },
-  {
-    id: 6,
-    src: "../images/Home/g-6.jpg",
-  },
-  {
-    id: 7,
-    src: "../images/Home/g-7.jpg",
-  },
-  {
-    id: 8,
-    src: "../images/Home/g-8.jpg",
-  },
-  {
-    id: 9,
-    src: "../images/Home/g-9.jpg",
-  },
-];
  class Home extends React.Component {
    constructor(props) {
      super(props);
